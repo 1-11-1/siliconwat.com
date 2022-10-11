110d9c56900d19d23cf43b184a793ab70600c976
Thon Ly <thonly@heartbank.ceo>
2022-10-10 19:15:42 -0700

    begin header

--- components/sw-header/template.mjs
+++ components/sw-header/template.mjs
@@ -10,44 +10,82 @@ template.innerHTML = `
         <nav>
             <ul>
                 <li>
-                    <h3>Hello</h3>
-                    <menu>
-                        <li>Hi</li>
-                        <li>Hi</li>
-                        <li>Hi</li>
-                    </menu>
+                    <h3>Frontend Course</h3>
+                    <div>
+                        <h4>Learn</h4>
+                        <menu>
+                            <li>Read Textbook on Medium</li>
+                            <li>Watch Videos on Udemy</li>
+                            <li>Hi</li>
+                        </menu>
+                        <h4>Practice</h4>
+                        <menu>
+                            <li>Coding Exercises</li>
+                            <li>Pair Programming</li>
+                            <li>Hi</li>
+                        </menu>
+                        <h4>Review</h4>
+                        <menu>
+                            <li>Flashcards</li>
+                            <li>Mock Interviews on YouTube</li>
+                            <li>Hi</li>
+                        </menu>
+                        <h4>Advanced Course</h4>
+                        <menu>
+                            <li>Web Audio Production</li>
+                        </menu>
+                    </div>
                 </li>
             </ul>
             <ul>
                 <li>
-                    <h3>Hello</h3>
-                    <menu>
-                        <li>Hi</li>
-                        <li>Hi</li>
-                        <li>Hi</li>
-                    </menu>
+                    <h3>Backend Course</h3>
+                    <div>
+                        <menu>
+                            <li>Hi</li>
+                            <li>Hi</li>
+                            <li>Hi</li>
+                        </menu>
+                    </div>
                 </li>
             </ul>
             <ul>
                 <li>
-                    <h3>Hello</h3>
-                    <menu>
-                        <li>Hi</li>
-                        <li>Hi</li>
-                        <li>Hi</li>
-                        <li>Hi</li>
-                    </menu>
+                    <h3>iOS Course</h3>
+                    <div>
+                        <menu>
+                            <li>Hi</li>
+                            <li>Hi</li>
+                            <li>Hi</li>
+                        </menu>
+                    </div>
                 </li>
             </ul>
             <ul>
                 <li>
-                    <h3>Hello</h3>
-                    <menu>
-                        <li>Hi</li>
-                        <li>Hi</li>
-                        <li>Hi</li>
-                        <li>Hi</li>
-                    </menu>
+                    <h3>Graduation</h3>
+                    <div>
+                        <h4>Inclusion & Diversity</h4>
+                        <menu>
+                            <li>Committment Deposit</li>
+                            <li>Certificate of Completion</li>
+                            <li>Apply for Mock Interview</li>
+                            <li>Apply for Admission to Campus</li>
+                        </menu>
+                    </div>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <h3>Coupons</h3>
+                    <div>
+                        <h4>Inclusion & Diversity</h4>
+                        <menu>
+                            <li>Women & Minorities</li>
+                            <li>Cambodians</li>
+                            <li>Ukrainian Refugees</li>
+                        </menu>
+                    </div>
                 </li>
             </ul>
         </nav>
